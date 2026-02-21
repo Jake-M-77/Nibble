8c97c864b5db7600315ffef0b6fd04709a264699
Jake M <jakemanson77@icloud.com>
2026-02-21 16:10:11 +0000

    feat: fetch recipes and render search results

--- Nibble/src/App.jsx
+++ Nibble/src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  const API_KEY = import.meta.env.VITE_SPOONACULAR_API_KEY;
+
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    try {
+      const resp = await fetch('https://api.spoonacular.com/recipes/complexSearch?cuisine=Latin American', 
+        {
+    headers: {'x-api-key': API_KEY}
+        } )
+    
+      const json = await resp.json();
+      setData(json);
+    
+    } catch (error) {
+      console.log("broken lol")
+    }
+  }
+
+  
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <>
       <h1>Hello, World!</h1>
+
+      <p>{API_KEY}</p>
+
+
+      {data?.results?.map(recipe => (
+        <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+        </div>
+      ))}
+      
+
     </>
   )
 }
